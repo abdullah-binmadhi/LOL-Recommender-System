d3e10cfb3e159e0265367a635bff4a8d4866f9dc
abdullah-binmadhi <89339365+abdullah-binmadhi@users.noreply.github.com>
2025-10-26 12:59:43 +0800

    Remove all Google Analytics and Vercel Analytics tracking
    • Removed Google Analytics code from all HTML files
    
    • Removed Vercel Analytics dependency from package.json
    
    • Removed Vercel Analytics component from Layout.js
    
    • Removed Analytics navigation link from Navigation.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,10 @@
 import Navigation from './Navigation'
-import { Analytics } from '@vercel/analytics/next'
 
 export default function Layout({ children }) {
   return (
     <div>
       <Navigation />
       <main>{children}</main>
-      <Analytics />
     </div>
   )
 }
